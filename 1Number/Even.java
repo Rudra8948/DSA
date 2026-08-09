b0fc01ac3cce1cf4ab21d2c4c1e90b98f7de5e25
Rudra Singh <asaditya0563@gmail.com>
2026-08-09 12:40:31 +0000

    Even/ODD number

---
+++ 1Number/Even.java
@@ -0,0 +1,15 @@
+//Check whether a number is even.
+import java.util.*;
+public class Even{
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.print("GIVEN NUMBER = ");
+        int number=sc.nextInt();
+        if(number%2==0){
+            System.out.println("EVEN");
+        }
+        else{
+            System.out.println("ODD");
+        }
+    }
+}
